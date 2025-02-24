cb3b3c1faf7e4ceb1b8d1e8e5f6bd33512133669
VladRoschupkin <rosupkinvladislav4@gmail.com>
2025-02-24 15:35:03 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menuToggle");
+    const menu = document.getElementById("menu");
+
+    menuToggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
+    });
+});
+import * as THREE from './three.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
